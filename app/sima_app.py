522a91d7223f274a0dd026d30f5191a1f4954937
Gerald Campos <geraldcamposm10@gmail.com>
2025-05-26 15:38:46 -0500

    agregando ip

--- app/sima_app.py
+++ app/sima_app.py
@@ -1,17 +1,30 @@
 #%% Libraries
 import streamlit as st
 import os
+import requests
 from datetime import datetime, timedelta
 import pytz
 from function_cocteles import coctel_dashboard, cargar_coctel_completo
 from function_users import usarios_acontecimientos_dashboard
 
-#%%% Formateo del nombre del sitio y site config
+#%% Crear funcion para saber el ip
+@st.cache_data(ttl=3600)
+def get_egress_ip():
+    try:
+        # api.ipify.org devuelve tu IP pública en texto plano
+        return requests.get("https://api.ipify.org").text.strip()
+    except Exception:
+        return "no disponible"
+    
+#%% Formateo del nombre del sitio y site config
 st.set_page_config(page_title="Dashboard SIMA", layout="centered")
+st.sidebar.title("Opciones")
+ip = get_egress_ip()
+st.sidebar.write(f"IP de salida: {ip}")
 st.title("Dashboard SIMA")
 st.divider()
 
-#%%%% Agregar opción al menú lateral
+#%%% Agregar opción al menú lateral
 # peru_tz = pytz.timezone("America/Lima")
 df_coctel, *_ = cargar_coctel_completo()
 if not df_coctel.empty:
@@ -26,7 +39,6 @@ if not df_coctel.empty:
 else:
     last_updated_date = "Sin datos disponibles"
 
-st.sidebar.title("Opciones")
 st.sidebar.write(f"Fecha de última actualización de datos: {last_updated_date}")
 menu = st.sidebar.radio(
     "Selecciona una sección",
